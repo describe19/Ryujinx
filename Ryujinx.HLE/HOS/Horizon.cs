bf7ddb13b0559bf1a4f417424412c14f5be65816
describe19 <nathanmvdh@gmail.com>
2020-04-23 15:24:52 +0930

    Revert "Merge branch 'pr/981' into testing"
    This reverts commit 553c82c389a31316b77c9e636bfdc803ae7ac2c1, reversing
    changes made to 14cbeb2056565edc1dbe8c4b78b3f1e0bd5f0f54.

--- Ryujinx.HLE/HOS/Horizon.cs
+++ Ryujinx.HLE/HOS/Horizon.cs
@@ -66,8 +66,6 @@ namespace Ryujinx.HLE.HOS
 
         internal Switch Device { get; private set; }
 
-        internal SurfaceFlinger SurfaceFlinger { get; private set; }
-
         public SystemStateMgr State { get; private set; }
 
         internal bool KernelInitialized { get; private set; }
@@ -268,8 +266,6 @@ namespace Ryujinx.HLE.HOS
             DatabaseImpl.Instance.InitializeDatabase(device);
 
             HostSyncpoint = new NvHostSyncpt(device);
-
-            SurfaceFlinger = new SurfaceFlinger(device);
         }
 
         public void LoadCart(string exeFsDir, string romFsFile = null)
@@ -852,8 +848,6 @@ namespace Ryujinx.HLE.HOS
             {
                 _isDisposed = true;
 
-                SurfaceFlinger.Dispose();
-
                 KProcess terminationProcess = new KProcess(this);
 
                 KThread terminationThread = new KThread(this);
@@ -876,6 +870,12 @@ namespace Ryujinx.HLE.HOS
                 });
 
                 terminationThread.Start();
+
+                // Signal the vsync event to avoid issues of KThread waiting on it.
+                if (Device.EnableDeviceVsync)
+                {
+                    Device.VsyncEvent.Set();
+                }
 
                 // Destroy nvservices channels as KThread could be waiting on some user events.
                 // This is safe as KThread that are likely to call ioctls are going to be terminated by the post handler hook on the SVC facade.
